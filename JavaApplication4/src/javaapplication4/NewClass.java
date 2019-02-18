4288c5aadc485c70928776a5085afc1b788e33bd
ieb <ieb@LAPTOP-IEB2.oxia.corp>
2019-02-18 15:15:37 +0100

    first

--- JavaApplication4/src/javaapplication4/NewClass.java
+++ JavaApplication4/src/javaapplication4/NewClass.java
@@ -5,10 +5,19 @@
  */
 package javaapplication4;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 /**
  *
  * @author ieb
  */
 public class NewClass {
-    private int i;
+  public static void main(String[] args) {
+    Result result = JUnitCore.runClasses(MyClassTest.class);
+    for (Failure failure : result.getFailures()) {
+      System.out.println(failure.toString());
+    }
+  }
 }
