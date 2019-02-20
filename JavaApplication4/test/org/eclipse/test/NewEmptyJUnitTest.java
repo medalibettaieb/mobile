144af8f96335f0f14f392d62ecc5971783c87476
ieb <ieb@LAPTOP-IEB2.oxia.corp>
2019-02-20 08:56:25 +0100

    first

---
+++ JavaApplication4/test/org/eclipse/test/NewEmptyJUnitTest.java
@@ -0,0 +1,21 @@
+package org.eclipse.test;
+import static org.junit.Assert.fail;
+import org.junit.jupiter.api.Test;
+class CalculTest {
+@BeforeAll
+static void setUpBeforeClass() throws Exception { }
+@AfterAll
+static void tearDownAfterClass() throws Exception { }
+@BeforeEach
+void setUp() throws Exception { }
+@AfterEach
+void tearDown() throws Exception { }
+@Test
+void testSomme() {
+fail("Not yet implemented");
+}
+@Test
+void testDivision() {
+fail("Not yet implemented");
+}
+}
